43cf0534bb39cfca8b4d5fe069ae5d244397baf8
NavzKroco <135635413+LordNavz@users.noreply.github.com>
2024-02-22 17:56:29 +0700

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,10 +1,10 @@
 const fs = require('fs')
 
-global.creator = 'Zeeoneofc'// yourname
-global.MONGO_DB_URI = "" //database mongodb 
+global.creator = 'Navz'// yourname
+global.MONGO_DB_URI = "mongodb+srv://navapi:navapi@navz.amk5zhr.mongodb.net/?retryWrites=true&w=majority&appName=Navz" //database mongodb 
 global.ACTIVATION_TOKEN_SECRET = "-@Pqnap+@(/1jAPPnew/@10" //isi apa aja bebas
-global.your_email = "" //email
-global.email_password = "" //application password email
+global.your_email = "navisxd31@gmail.com" //email
+global.email_password = "mmzx pyqa xtwy hltz" //application password email
 global.limitCount = 10000
 global.YUOR_PORT = 8000
 global.loghandler = {
